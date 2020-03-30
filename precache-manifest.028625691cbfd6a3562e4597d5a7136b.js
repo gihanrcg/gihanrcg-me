8215b410ef96e99211ff54c8f393e6149debbe36
Gihan Siriwardhana <gihan.siriwardhana@ifsworld.com>
2020-03-30 18:42:19 +0530

    Updates

---
+++ precache-manifest.028625691cbfd6a3562e4597d5a7136b.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae80401c2bc55a792adeada95db0a22",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "b676834e7eceeebb399c",
+    "url": "/me/static/css/2.aeedae4c.chunk.css"
+  },
+  {
+    "revision": "7aad84958e78cf2f3c77",
+    "url": "/me/static/css/main.7ce9afb0.chunk.css"
+  },
+  {
+    "revision": "b676834e7eceeebb399c",
+    "url": "/me/static/js/2.43e81358.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/me/static/js/2.43e81358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aad84958e78cf2f3c77",
+    "url": "/me/static/js/main.ddb51344.chunk.js"
+  },
+  {
+    "revision": "5c3aeb7f6dd833dc062f",
+    "url": "/me/static/js/runtime-main.165e819d.js"
+  },
+  {
+    "revision": "92339a20974af6ec69d0361a3bbff09b",
+    "url": "/me/static/media/12 (1).92339a20.jpg"
+  },
+  {
+    "revision": "be0b1de05f34c7dbc010cb7b412f100c",
+    "url": "/me/static/media/12 (2).be0b1de0.jpg"
+  },
+  {
+    "revision": "d6076dc825a448fa5c5854611cb7da16",
+    "url": "/me/static/media/12 (3).d6076dc8.jpg"
+  },
+  {
+    "revision": "2dcdd0294d63ed4b2f17f2b6951b7b59",
+    "url": "/me/static/media/12 (4).2dcdd029.jpg"
+  },
+  {
+    "revision": "6e92f8b81fb67a75c6ce45cafda368e5",
+    "url": "/me/static/media/12 (5).6e92f8b8.jpg"
+  },
+  {
+    "revision": "3c3f17adbb8e8a01948a0d76dfe59534",
+    "url": "/me/static/media/12 (6).3c3f17ad.jpg"
+  },
+  {
+    "revision": "ee492cd6b1c93ea68e360cb4a5b07ee8",
+    "url": "/me/static/media/12 (7).ee492cd6.jpg"
+  },
+  {
+    "revision": "ea97311deec4078908ea2b716480e1ae",
+    "url": "/me/static/media/12 (8).ea97311d.jpg"
+  },
+  {
+    "revision": "66654804410be8a26610992d433fab9b",
+    "url": "/me/static/media/41486278_617028988699144_2494795976527577088_o.66654804.jpg"
+  },
+  {
+    "revision": "4e13a77a37c14103b8ca4b4de5295853",
+    "url": "/me/static/media/55613120_726260424442666_7850560511774031872_n.4e13a77a.jpg"
+  },
+  {
+    "revision": "88daba7ebebc1eb5d6579154d9147931",
+    "url": "/me/static/media/55897189_726262351109140_2032770848254853120_o.88daba7e.jpg"
+  },
+  {
+    "revision": "792a97297258521a0a5b7bd86a730a31",
+    "url": "/me/static/media/89774187_2872124749515244_3824864798827347968_n.792a9729.jpg"
+  },
+  {
+    "revision": "6f672ed9af55f66c29b5e91620136982",
+    "url": "/me/static/media/89809696_2872124559515263_2552127334593331200_n.6f672ed9.jpg"
+  },
+  {
+    "revision": "778ce50fefd334b7d4c3ecf062ee82ca",
+    "url": "/me/static/media/AGM1.778ce50f.jpg"
+  },
+  {
+    "revision": "d00443cd9e1f56e74a6e4fa9a7934a49",
+    "url": "/me/static/media/AGM2.d00443cd.jpg"
+  },
+  {
+    "revision": "bf52e47bbc58614107da407bb2f244ee",
+    "url": "/me/static/media/blog.bf52e47b.JPG"
+  },
+  {
+    "revision": "e321c6a9f42627259417769215227f51",
+    "url": "/me/static/media/britishcouncil.e321c6a9.jpg"
+  },
+  {
+    "revision": "b20fa477dd990b05c28e8efb1ff4efad",
+    "url": "/me/static/media/commitee.b20fa477.jpg"
+  },
+  {
+    "revision": "a7cda3ca009dee946512e6c4cbbc79fb",
+    "url": "/me/static/media/covid.a7cda3ca.JPG"
+  },
+  {
+    "revision": "92d238ddaf6e9f1a941ef6acb4c9558e",
+    "url": "/me/static/media/expalining2.92d238dd.jpg"
+  },
+  {
+    "revision": "5e6a0610e0210a109a4131d70c52a25b",
+    "url": "/me/static/media/explain_miniHackathon.5e6a0610.jpg"
+  },
+  {
+    "revision": "9158410b36a09801d3691a0b5b0f871a",
+    "url": "/me/static/media/explainingrules.9158410b.jpg"
+  },
+  {
+    "revision": "8e1a562ab02efbc91f60c31954d2306b",
+    "url": "/me/static/media/gihan.8e1a562a.jpg"
+  },
+  {
+    "revision": "bc741a74a5d2e8d085fa773abb164c2e",
+    "url": "/me/static/media/hotel.bc741a74.png"
+  },
+  {
+    "revision": "9098efb401a322d8d608e5577e2a2f28",
+    "url": "/me/static/media/inventory.9098efb4.JPG"
+  },
+  {
+    "revision": "a08fb8cb074cf94a215bf6fecfc13196",
+    "url": "/me/static/media/karate.a08fb8cb.JPG"
+  },
+  {
+    "revision": "ca388046d05826c0461d078629fa7328",
+    "url": "/me/static/media/kk.ca388046.jpg"
+  },
+  {
+    "revision": "2e75fdfb06dca57421c9463dbac73dbf",
+    "url": "/me/static/media/kkk.2e75fdfb.jpg"
+  },
+  {
+    "revision": "5853db0bbf6a1f70dbecdf3761c6d034",
+    "url": "/me/static/media/mentoring_miniHackathon.5853db0b.jpg"
+  },
+  {
+    "revision": "142ef3fad45964295242c0e41935d6a6",
+    "url": "/me/static/media/miniHackathon_commitee.142ef3fa.jpg"
+  },
+  {
+    "revision": "a2191803af25dd1d220e12a5ca6a12c7",
+    "url": "/me/static/media/ms champs participants.a2191803.jpg"
+  },
+  {
+    "revision": "659a26dbae62c2bc3628be3a2e4e41aa",
+    "url": "/me/static/media/oillamp.659a26db.jpg"
+  },
+  {
+    "revision": "05032f60c93ff3437c20d9db87596ad8",
+    "url": "/me/static/media/openday.05032f60.jpg"
+  },
+  {
+    "revision": "19e87fd291f1214f6b37b6d1fb09c39b",
+    "url": "/me/static/media/openday2.19e87fd2.jpg"
+  },
+  {
+    "revision": "306f0d0f56058f12d8f28c61604665b4",
+    "url": "/me/static/media/schoolmanagementsystem.306f0d0f.JPG"
+  },
+  {
+    "revision": "d6613e2bba9c4d38f3bc7b8d5a556689",
+    "url": "/me/static/media/step into cloud.d6613e2b.jpg"
+  },
+  {
+    "revision": "820172011ded80e3e23288569818d491",
+    "url": "/me/static/media/tokenof_appriciation.82017201.jpg"
+  },
+  {
+    "revision": "9877e9b9fe21a14f43ca5d4e0c2cd98b",
+    "url": "/me/static/media/zzz.9877e9b9.jpg"
+  },
+  {
+    "revision": "923fefe36d69d11b374517bab7b0acda",
+    "url": "/me/static/media/zzzz.923fefe3.jpg"
+  },
+  {
+    "revision": "19d18c48447d71cd264767d5e1f583c5",
+    "url": "/me/static/media/zzzzz.19d18c48.jpg"
+  }
+]);
